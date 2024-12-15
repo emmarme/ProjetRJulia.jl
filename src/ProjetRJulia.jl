cd522f46d61508706854607b00a9816f91f26b81
Kameyagi <mambas.jade@gmail.com>
2024-12-15 19:12:02 +0100

    ajout fonction

--- src/ProjetRJulia.jl
+++ src/ProjetRJulia.jl
@@ -1,6 +1,6 @@
 module ProjetRJulia
 
-export load_data, train_model, evaluate_model, load_data2, train_model2, evaluate_model2
+export load_data, train_model, evaluate_model, load_data2, train_model2, evaluate_model2, init_snake, init_snake2, init_long_snake2, spawn_food, spawn_food2, draw_game, draw_game2, move_snake, move_snake2, check_collision, check_collision2, get_input, get_input2, game_loop, game_loop2
 
 include("breastcancer.jl")
 
